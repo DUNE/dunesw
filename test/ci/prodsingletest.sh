c1603d0957d0618d62981e0fecfc442755f90f93
Eric Church <echurch@fnal.gov>
2014-11-05 07:04:30 -0600

    change names on git tests

--- test/ci/prodsingletest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Test LArSoft code with "prodsingle.fcl".
-
-
-cp  ${LBNECODE_DIR}/job/prodsingle_lbne35t.fcl .
-strace -o lar.strace lar -c ${LBNECODE_DIR}/job/prodsingle_lbne35t.fcl -n 1 -o single_gen.root -T single_hist.root
