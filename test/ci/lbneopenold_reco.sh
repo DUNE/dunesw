16d55b8e849a352a3cd90e1fc1bdebb6c4a6468e
Eric Church <echurch@fnal.gov>
2014-11-26 12:54:16 -0600

    Pull the correct lbne35t reco fcl file

--- test/ci/lbneopenold_reco.sh
+++ test/ci/lbneopenold_reco.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # echurch@fnal.gov
 
-cp  ${LBNECODE_DIR}/job/standard_reco_lbne.fcl .
+cp  ${LBNECODE_DIR}/job/standard_reco_lbne35t.fcl .
 #echo "services.user.FileCatalogMetadataExtras.RenameTemplate: '' " >> ./standard_reco_lbne_2D.fcl
-echo "outputs.out1.fileName: 'openclose_reco_lbne.root'" >> ./standard_reco_lbne.fcl
-lar --process-name citest-reco -c standard_reco_lbne.fcl -s ../lar_ci_openold_detsim_lbnecode/openclose_detsim_lbne.root -n 1 -o openclose_reco_lbne.root -T openclose_reco_hist_lbne.root
+echo "outputs.out1.fileName: 'openclose_reco_lbne.root'" >> ./standard_reco_lbne35t.fcl
+lar --process-name citest-reco -c standard_reco_lbne35t.fcl -s ../lar_ci_openold_detsim_lbnecode/openclose_detsim_lbne.root -n 1 -o openclose_reco_lbne.root -T openclose_reco_hist_lbne.root
