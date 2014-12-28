5cf2532b70d1293168856fa1cc83c565c277f259
Eric Church <echurch@fnal.gov>
2014-12-28 12:11:55 -0600

    Create an lbne35t minimal reco CI test to run after the already-existing detsim test. This runs caldata, gaushit, hitcheat, fuzzycluster. Note that we have to set the TPC TimingOffset in these two jobs to -1600, due to the fact that the anu files I'm running over were configured to run that way (which may have been inadvertent, dunno).

--- test/ci/lbneopenold_reco.sh
+++ test/ci/lbneopenold_reco.sh
@@ -3,5 +3,6 @@
 
 cp  ${LBNECODE_DIR}/job/minimal_reco_lbne35t.fcl .
 #echo "services.user.FileCatalogMetadataExtras.RenameTemplate: '' " >> ./minimal_reco_lbne_2D.fcl
+echo "services.user.TimeService.TriggerOffsetTPC: -1600" >> ./minimal_reco_lbne35t.fcl # for consistency
 echo "outputs.out1.fileName: 'openclose_reco_lbne.root'" >> ./minimal_reco_lbne35t.fcl
 lar --process-name citest-reco -c minimal_reco_lbne35t.fcl -s ../lar_ci_openold_detsim_lbnecode/openclose_detsim_lbne.root -n 1 -o openclose_reco_lbne.root -T openclose_reco_hist_lbne.root
