7eef1ce5cc3c2fff9894b85e6aff64df97179fa4
Eric Church <echurch@fnal.gov>
2015-01-02 11:33:21 -0600

    standard_reco_35t.fcl is not in develop yet, so return to to minmal_reco_35t.fcl

--- test/ci/lbneopenold_reco.sh
+++ test/ci/lbneopenold_reco.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # echurch@fnal.gov
 
-cp  ${LBNECODE_DIR}/job/standard_reco_lbne35t.fcl .
-#echo "services.user.FileCatalogMetadataExtras.RenameTemplate: '' " >> ./standard_reco_lbne_2D.fcl
-echo "services.user.TimeService.TriggerOffsetTPC: -1600" >> ./standard_reco_lbne35t.fcl # for consistency
-echo "outputs.out1.fileName: 'openclose_reco_lbne.root'" >> ./standard_reco_lbne35t.fcl
-lar --process-name citest-reco -c ./standard_reco_lbne35t.fcl -s ../lar_ci_openold_detsim_lbnecode/openclose_detsim_lbne.root -n 1 -o openclose_reco_lbne.root -T openclose_reco_hist_lbne.root
+cp  ${LBNECODE_DIR}/job/minimal_reco_lbne35t.fcl .
+#echo "services.user.FileCatalogMetadataExtras.RenameTemplate: '' " >> ./minimal_reco_lbne_2D.fcl
+echo "services.user.TimeService.TriggerOffsetTPC: -1600" >> ./minimal_reco_lbne35t.fcl # for consistency
+echo "outputs.out1.fileName: 'openclose_reco_lbne.root'" >> ./minimal_reco_lbne35t.fcl
+lar --process-name citest-reco -c ./minimal_reco_lbne35t.fcl -s ../lar_ci_openold_detsim_lbnecode/openclose_detsim_lbne.root -n 1 -o openclose_reco_lbne.root -T openclose_reco_hist_lbne.root
