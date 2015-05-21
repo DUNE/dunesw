af5ef12c53e75c2e059b5ab8c9173eec1258c9a3
Marc Mengel <mengel@fnal.gov>
2015-05-20 22:01:02 -0500

    sed macos vs linux fix

--- test/ci/lbneopenold_detsim.sh
+++ test/ci/lbneopenold_detsim.sh
@@ -12,10 +12,10 @@ echo "source.maxEvents: -1" >> ./prodsingle_lbne35t.fcl
 echo "services.user.TimeService.TriggerOffsetTPC: -1600" >> ./prodsingle_lbne35t.fcl # for consistency with the (probably inadvertently set) parameter in file at /pnfs/lbne/mc/lbne/simulated/002/singleparticle_antimu_20141105_Simulation009.root that this test reads
 
 # We want to just run detsim in this job
-sed -e '/generator/s/^/#/g' -i prodsingle_lbne35t.fcl 
-sed -e '/largeant/s/^/#/g'  -i prodsingle_lbne35t.fcl 
+sed -e '/generator/s/^/#/g' -i.bak prodsingle_lbne35t.fcl 
+sed -e '/largeant/s/^/#/g'  -i.bak prodsingle_lbne35t.fcl 
 # These add lines back in
-sed -i '/simulate\: /asimulate\: [ daq, rns ]' -i prodsingle_lbne35t.fcl
-sed -i '/RandomNumberGenerator/a  RandomNumberGenerator\: \{\} \# Added back ' -i prodsingle_lbne35t.fcl
+sed -e '/simulate\: /asimulate\: [ daq, rns ]' -i.bak prodsingle_lbne35t.fcl
+sed -e '/RandomNumberGenerator/a  RandomNumberGenerator\: \{\} \# Added back ' -i.bak prodsingle_lbne35t.fcl
 
 lar --process-name citest-detsim -c prodsingle_lbne35t.fcl -s $1 -n 1 -o openclose_detsim_lbne.root -T openclose_detsim_hist_lbne.root
