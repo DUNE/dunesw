c324942fe0bffbe8052f8b2911a4be321c74d44b
Vito Di Benedetto <vito@fnal.gov>
2015-09-21 16:05:47 -0500

    Updated CI regression test

--- test/ci/ci_regression_test_dunetpc.sh
+++ test/ci/ci_regression_test_dunetpc.sh
@@ -3,21 +3,34 @@
 NEVENTS=$1
 STEP=$2
 LARSOFT_REFERENCE_VERSION=$3
-BASEFILENAME="AntiMuonCutEvents_LSU_v2_dune35t"
-EXPCODE="dunetpc"
+BASEFILENAME=$4
+EXPCODE=$5
 
-declare -a STEPS=("" "gen" "geant" "detsim" "reco" "ana")
+declare -a STEPS=( ${@:6:$(($#-5))} )
 
-REFERENCE_FILE="${BASEFILENAME}_Reference_${STEPS[STEP]}_${LARSOFT_REFERENCE_VERSION}.root"
-if [ $STEP -eq 0 ]; then REFERENCE_FILE=""; fi
+
+INPUT_FILE="${BASEFILENAME}_Reference_${STEPS[STEP-1]}_${LARSOFT_REFERENCE_VERSION}.root"
+if [ x"${STEPS[STEP-1]}" == xnone ]; then INPUT_FILE=""; fi
+
+OUTPUT_FILE="${BASEFILENAME}_Current_${STEPS[STEP]}.root"
+
+FHiCL_FILE="ci_test_${STEPS[STEP]}_${EXPCODE}.fcl"
+
+echo "Input file:  $INPUT_FILE"
+echo "Output file: $OUTPUT_FILE"
+echo "FHiCL file:  $FHiCL_FILE"
+echo
+
 
 
 function larsoft_data_production
 {
 
-    echo -e "\nNumber of events for ${STEPS[STEP+1]} step: $NEVENTS\n"
+    echo -e "\nNumber of events for ${STEPS[STEP]} step: $NEVENTS\n"
+    echo lar --rethrow-default -n $NEVENTS -o $OUTPUT_FILE --config $FHiCL_FILE ${INPUT_FILE}
+    echo
 
-    lar --rethrow-default -n $NEVENTS -o ${BASEFILENAME}_Current_${STEPS[STEP+1]}.root --config ci_test_${STEPS[STEP+1]}_${EXPCODE}.fcl ${REFERENCE_FILE}
+    lar --rethrow-default -n $NEVENTS -o $OUTPUT_FILE --config $FHiCL_FILE ${INPUT_FILE}
 
 }
 
@@ -29,23 +42,22 @@ function compare_data_products
 
     if [ $COMPAREINIT -eq 0 ]; then
 
-        lar --rethrow-default -n $NEVENTS --config eventdump.fcl ${BASEFILENAME}_Reference_${STEPS[STEP+1]}_${LARSOFT_REFERENCE_VERSION}.root > ${BASEFILENAME}_Reference_${STEPS[STEP+1]}.dump
-        OUTPUT_REFERENCE=$(cat ${BASEFILENAME}_Reference_${STEPS[STEP+1]}.dump | sed -e  '/PROCESS NAME/,/^\s*$/!d ; s/PROCESS NAME.*$// ; /^\s*$/d' )
+        lar --rethrow-default -n $NEVENTS --config eventdump.fcl ${BASEFILENAME}_Reference_${STEPS[STEP]}_${LARSOFT_REFERENCE_VERSION}.root > ${BASEFILENAME}_Reference_${STEPS[STEP]}.dump
+        OUTPUT_REFERENCE=$(cat ${BASEFILENAME}_Reference_${STEPS[STEP]}.dump | sed -e  '/PROCESS NAME/,/^\s*$/!d ; s/PROCESS NAME.*$// ; /^\s*$/d' )
 
-        lar --rethrow-default -n $NEVENTS --config eventdump.fcl ${BASEFILENAME}_Current_${STEPS[STEP+1]}.root > ${BASEFILENAME}_Current_${STEPS[STEP+1]}.dump
-        OUTPUT_CURRENT=$(cat ${BASEFILENAME}_Current_${STEPS[STEP+1]}.dump | sed -e  '/PROCESS NAME/,/^\s*$/!d ; s/PROCESS NAME.*$// ; /^\s*$/d' )
+        lar --rethrow-default -n $NEVENTS --config eventdump.fcl ${BASEFILENAME}_Current_${STEPS[STEP]}.root > ${BASEFILENAME}_Current_${STEPS[STEP]}.dump
+        OUTPUT_CURRENT=$(cat ${BASEFILENAME}_Current_${STEPS[STEP]}.dump | sed -e  '/PROCESS NAME/,/^\s*$/!d ; s/PROCESS NAME.*$// ; /^\s*$/d' )
 
         echo -e "\nCompare data products."
-        echo    "Reference files for ${STEPS[STEP+1]} step generated using LARSOFT_VERSION $LARSOFT_REFERENCE_VERSION"
-        echo -e "Current files for ${STEPS[STEP+1]} step generated using LARSOFT_VERSION $LARSOFT_VERSION\n"
-        echo -e "\n${BASEFILENAME}_Reference_${STEPS[STEP+1]}.dump\n"
+        echo    "Reference files for ${STEPS[STEP]} step generated using LARSOFT_VERSION $LARSOFT_REFERENCE_VERSION"
+        echo -e "Current files for ${STEPS[STEP]} step generated using LARSOFT_VERSION $LARSOFT_VERSION\n"
+        echo -e "\n${BASEFILENAME}_Reference_${STEPS[STEP]}.dump\n"
         echo "$OUTPUT_REFERENCE"
-        echo -e "\n${BASEFILENAME}_Current_${STEPS[STEP+1]}.dump\n"
+        echo -e "\n${BASEFILENAME}_Current_${STEPS[STEP]}.dump\n"
         echo "$OUTPUT_CURRENT"
     fi
 
-
-    DIFF=$(diff  <(echo "$OUTPUT_REFERENCE" | awk -v MYNF=$((1-$1)) 'NF{NF-=MYNF}1' ) <(echo "$OUTPUT_CURRENT" | awk -v MYNF=$((1-$1)) 'NF{NF-=MYNF}1' ) )
+    DIFF=$(diff  <(echo "$OUTPUT_REFERENCE" | awk -v MYNF=$((1-$1)) 'NF{NF-=MYNF}1' | sed 's/\.//g' ) <(echo "$OUTPUT_CURRENT" | awk -v MYNF=$((1-$1)) 'NF{NF-=MYNF}1' | sed 's/\.//g' ) )
 
     echo
     echo ${CHECKMSG[$1]}
@@ -54,7 +66,7 @@ function compare_data_products
 
     if [ ${#DIFF} -gt 0 ]; then
        echo "$DIFF"
-       exit 1;
+       exitstatus 1 "compare_data_products step $1";
     else
        echo -e "none\n\n"
     fi
@@ -63,13 +75,31 @@ function compare_data_products
 
 }
 
-trap 'LASTERR=$?; echo `basename $0`: line ${LINENO}: exit status: ${LASTERR}; exit ${LASTERR}' ERR
+function exitstatus
+{
+    EXITSTATUS=$1
+    TASKSTRING=$2
+
+    echo -e "\nCI MSG $TASKSTRING exit status: ${EXITSTATUS}\n"
+    if [ ${EXITSTATUS} -ne 0 ]; then
+      exit ${EXITSTATUS}
+    fi
+}
+
+trap 'LASTERR=$?; echo -e "\nCI MSG `basename $0`: line ${LINENO}: exit status: ${LASTERR}\n"; exit ${LASTERR}' ERR
 
 echo -e "\nRun `basename $0` script"
 
 larsoft_data_production
 
+exitstatus $? "larsoft_data_production"
+
+
 COMPAREINIT=0
 compare_data_products 0 #Check for added/removed data products
 
+exitstatus $? "compare_data_products step 0"
+
 # compare_data_products 1 #Check for differences in the size of data products
+
+# exitstatus $? "compare_data_products step 1"
