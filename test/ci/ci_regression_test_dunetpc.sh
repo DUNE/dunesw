45c1c5cbbf472cdba1c789e72198f06959339825
Vito Di Benedetto <vito@fnal.gov>
2015-09-28 05:28:46 -0500

    Improved exit code handling

--- test/ci/ci_regression_test_dunetpc.sh
+++ test/ci/ci_regression_test_dunetpc.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 
-NEVENTS=$1
-STEP=$2
-LARSOFT_REFERENCE_VERSION=$3
-BASEFILENAME=$4
-EXPCODE=$5
+declare -a STEPS=( ${@:6:$(($#-5))} ) # -g option to declare this array global
+                                      # is not present in bash version 4.1.2(1)-release
+                                      # so the array has been declared in the global scope
 
-declare -a STEPS=( ${@:6:$(($#-5))} )
+function initialize
+{
+    trap 'LASTERR=$?; echo -e "\nCI MSG BEGIN\n `basename $0`: error at line ${LINENO}\n Stage: ${STEPS[STEP]}\n Task: ${TASKSTRING}\n exit status: ${LASTERR}\nCI MSG END\n"; exit ${LASTERR}' ERR
 
+    echo "initialize $@"
 
-INPUT_FILE="${BASEFILENAME}_Reference_${STEPS[STEP-1]}_${LARSOFT_REFERENCE_VERSION}.root"
-if [ x"${STEPS[STEP-1]}" == xnone ]; then INPUT_FILE=""; fi
+    NEVENTS=$1
+    STEP=$2
+    LARSOFT_REFERENCE_VERSION=$3
+    BASEFILENAME=$4
+    EXPCODE=$5
 
-OUTPUT_FILE="${BASEFILENAME}_Current_${STEPS[STEP]}.root"
+    #declare -ga STEPS=( ${@:6:$(($#-5))} )
 
-FHiCL_FILE="ci_test_${STEPS[STEP]}_${EXPCODE}.fcl"
+    INPUT_FILE="${BASEFILENAME}_Reference_${STEPS[STEP-1]}_${LARSOFT_REFERENCE_VERSION}.root"
+    if [ x"${STEPS[STEP-1]}" == xnone ]; then INPUT_FILE=""; fi
 
-echo "Input file:  $INPUT_FILE"
-echo "Output file: $OUTPUT_FILE"
-echo "FHiCL file:  $FHiCL_FILE"
-echo
+    OUTPUT_FILE="${BASEFILENAME}_Current_${STEPS[STEP]}.root"
 
+    FHiCL_FILE="ci_test_${STEPS[STEP]}_${EXPCODE}.fcl"
+
+    echo "Input file:  $INPUT_FILE"
+    echo "Output file: $OUTPUT_FILE"
+    echo "FHiCL file:  $FHiCL_FILE"
+    echo
+    echo -e "\nRunning\n `basename $0` $@"
+
+}
 
 
 function larsoft_data_production
 {
 
+    trap 'LASTERR=$?; echo -e "\nCI MSG BEGIN\n `basename $0`: error at line ${LINENO}\n Stage: ${STEPS[STEP]}\n Task: ${TASKSTRING}\n exit status: ${LASTERR}\nCI MSG END\n"; exit ${LASTERR}' ERR
+
     echo -e "\nNumber of events for ${STEPS[STEP]} step: $NEVENTS\n"
-    echo lar --rethrow-default -n $NEVENTS -o $OUTPUT_FILE --config $FHiCL_FILE ${INPUT_FILE}
+    echo lar --rethrow-default -n ${NEVENTS} -o ${OUTPUT_FILE} --config ${FHiCL_FILE} ${INPUT_FILE}
     echo
 
     lar --rethrow-default -n $NEVENTS -o $OUTPUT_FILE --config $FHiCL_FILE ${INPUT_FILE}
@@ -36,14 +49,19 @@ function larsoft_data_production
 
 function compare_data_products
 {
+    trap 'LASTERR=$?; echo -e "\nCI MSG BEGIN\n `basename $0`: error at line ${LINENO}\n Stage: ${STEPS[STEP]}\n Task: ${TASKSTRING}\n exit status: ${LASTERR}\nCI MSG END\n"; exit ${LASTERR}' ERR
 
     declare -a CHECKMSG=("Check for added/removed data products" "Check for differences in the size of data products")
 
 
-    if [ $COMPAREINIT -eq 0 ]; then
+    if [ ${COMPAREINIT} -eq 0 ]; then
+
+        #echo lar --rethrow-default -n $NEVENTS --config eventdump.fcl ${BASEFILENAME}_Reference_${STEPS[STEP]}_${LARSOFT_REFERENCE_VERSION}.root
 
         lar --rethrow-default -n $NEVENTS --config eventdump.fcl ${BASEFILENAME}_Reference_${STEPS[STEP]}_${LARSOFT_REFERENCE_VERSION}.root > ${BASEFILENAME}_Reference_${STEPS[STEP]}.dump
         OUTPUT_REFERENCE=$(cat ${BASEFILENAME}_Reference_${STEPS[STEP]}.dump | sed -e  '/PROCESS NAME/,/^\s*$/!d ; s/PROCESS NAME.*$// ; /^\s*$/d' )
+
+        #echo lar --rethrow-default -n $NEVENTS --config eventdump.fcl ${BASEFILENAME}_Current_${STEPS[STEP]}.root
 
         lar --rethrow-default -n $NEVENTS --config eventdump.fcl ${BASEFILENAME}_Current_${STEPS[STEP]}.root > ${BASEFILENAME}_Current_${STEPS[STEP]}.dump
         OUTPUT_CURRENT=$(cat ${BASEFILENAME}_Current_${STEPS[STEP]}.dump | sed -e  '/PROCESS NAME/,/^\s*$/!d ; s/PROCESS NAME.*$// ; /^\s*$/d' )
@@ -57,7 +75,11 @@ function compare_data_products
         echo "$OUTPUT_CURRENT"
     fi
 
-    DIFF=$(diff  <(echo "$OUTPUT_REFERENCE" | awk -v MYNF=$((1-$1)) 'NF{NF-=MYNF}1' | sed 's/\.//g' ) <(echo "$OUTPUT_CURRENT" | awk -v MYNF=$((1-$1)) 'NF{NF-=MYNF}1' | sed 's/\.//g' ) )
+    trap - ERR
+
+    DIFF=$(diff  <(echo "${OUTPUT_REFERENCE}" | awk -v MYNF=$((1-$1)) 'NF{NF-=MYNF}1' | sed 's/\.//g' ) <(echo "${OUTPUT_CURRENT}" | awk -v MYNF=$((1-$1)) 'NF{NF-=MYNF}1' | sed 's/\.//g' ) )
+
+    trap 'LASTERR=$?; echo -e "\nCI MSG BEGIN\n `basename $0`: error at line ${LINENO}\n Stage: ${STEPS[STEP]}\n Task: ${TASKSTRING}\n exit status: ${LASTERR}\nCI MSG END\n"; exit ${LASTERR}' ERR
 
     echo
     echo ${CHECKMSG[$1]}
@@ -65,8 +87,8 @@ function compare_data_products
     echo
 
     if [ ${#DIFF} -gt 0 ]; then
-       echo "$DIFF"
-       exitstatus 1999 "compare_data_products step $1";
+       echo "${DIFF}"
+       exitstatus $((2000+$1)) "$FUNCNAME step $1";
     else
        echo -e "none\n\n"
     fi
@@ -78,28 +100,33 @@ function compare_data_products
 function exitstatus
 {
     EXITSTATUS=$1
-    TASKSTRING=$2
 
-    echo -e "\nCI MSG $TASKSTRING exit status: ${EXITSTATUS}\n"
+    echo -e "\nCI MSG BEGIN\n Stage: ${STEPS[STEP]}\n Task: ${TASKSTRING}\n exit status: ${EXITSTATUS}\nCI MSG END\n"
     if [ ${EXITSTATUS} -ne 0 ]; then
       exit ${EXITSTATUS}
     fi
 }
 
-trap 'LASTERR=$?; echo -e "\nCI MSG `basename $0`: line ${LINENO}: exit status: ${LASTERR}\n"; exit ${LASTERR}' ERR
+trap 'LASTERR=$?; echo -e "\nCI MSG BEGIN\n `basename $0`: error at line ${LINENO}\n Stage: ${STEPS[STEP]}\n Task: ${TASKSTRING}\n exit status: ${LASTERR}\nCI MSG END\n"; exit ${LASTERR}' ERR
 
-echo -e "\nRun `basename $0` script"
+TASKSTRING="initialize"
+initialize $@
 
+exitstatus $?
+
+TASKSTRING="larsoft_data_production"
 larsoft_data_production
 
-exitstatus $? "larsoft_data_production"
+exitstatus $?
 
 
 COMPAREINIT=0
+TASKSTRING="compare_data_products 0"
 compare_data_products 0 #Check for added/removed data products
 
-exitstatus $? "compare_data_products step 0"
+exitstatus $?
 
-# compare_data_products 1 #Check for differences in the size of data products
+TASKSTRING="compare_data_products 1"
+compare_data_products 1 #Check for differences in the size of data products
 
-# exitstatus $? "compare_data_products step 1"
+exitstatus $?
