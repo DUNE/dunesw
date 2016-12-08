5565d86a9bb5e310277809a2ad4a4a89b96d7059
Thomas Karl Warburton <k.warburton@sheffield.ac.uk>
2016-12-08 15:38:14 +0000

    Renaming the MCTest fcl files.

--- fcl/dune35t/MCTests/MakeSimNoiseFcls.sh
+++ fcl/dune35t/MCTests/MakeSimNoiseFcls.sh
@@ -4,6 +4,7 @@
 BaseLifetime="3000"
 BaseField="[ 0.5, 0.782, 1.734 ]"
 BaseDiffusion="6.2e-9"
+BaseNoise="3.16"
 
 ### Declare the electron lifetime arrays
 declare -a Lifetimes=("1000" "2000" "3000" "5000" "8000")
@@ -16,6 +17,11 @@ declare -a FieldNames=("250V" "375V" "500V")
 ### Delcare the diffusion const arrays
 declare -a Diffusion=("0" "3.1e-9" "6.2e-9" "12.4e-9")
 declare -a DiffNames=("0Diff" "50Diff" "100Diff" "200Diff")
+
+### Delcare the noise normalisation arrays
+declare -a NoiseLevel=("3.16" "6.32" "9.48" "12.64")
+declare -a HitThresh=("[ 6, 6, 6 ]" "[ 10, 10, 10 ]" "[ 15, 15, 15 ]" "[ 20, 20, 20 ]")
+declare -a NoiseNames=("100N" "200N" "300N" "400N")
 
 ### Check that the length of the two electron lifetime arrays are equal
 LifeLen=${#Lifetimes[@]}
@@ -38,6 +44,14 @@ if [ $DiffLen -ne $DNamLen ]; then
     echo "The size of Diffusion is ${DiffLen} and the size of DiffNames is ${DNamLen}.....Fix it"
     exit
 fi
+### Check that the length of the two electron field arrays are equal
+NoiseLen=${#NoiseLevel[@]}
+HitThLen=${#HitThresh[@]}
+NoNamLen=${#NoiseNames[@]}
+if [[ $NoiseLen -ne $NoNamLen || $NoiseLen -ne $HitThLen || $HitThLen -ne $NoNamLen ]]; then
+    echo "The size of NoiseLevel is ${NoiseLen} the size of HitThresh is ${HitThLen} and the size of NoiseNames is ${NoNamLen}.....Fix it"
+    exit
+fi
 
 ### Define where I am going to put the fcl files
 MCTestFclDir=$MRB_SOURCE/dunetpc/fcl/dune35t/MCTests/
@@ -48,10 +62,12 @@ function MakeTheFcls(){
     ThisLife=$BaseLifetime
     ThisField=$BaseField
     ThisDiff=$BaseDiffusion
-    
+    ThisNoise=$BaseNoise
+
     LifeName="3ms"
     FieldName="500V"
     DiffName="100Diff"
+    NoiseName="100N"
 
     if [ $1 -eq 1 ]; then
 	ThisLife=${Lifetimes[$2-1]}
@@ -62,15 +78,19 @@ function MakeTheFcls(){
     elif [ $1 -eq 3 ]; then
 	ThisDiff=${Diffusion[$2-1]}
 	DiffName=${DiffNames[$2-1]}
+    elif [ $1 -eq 4 ]; then
+	ThisNoise=${NoiseLevel[$2-1]}
+	ThisHitTh=${HitThresh[$2-1]}
+	NoiseName=${NoiseNames[$2-1]}
     else
 	echo "Something is wrong, you're asking for parameter "$2
     fi
 
-    echo "Making files for ${ThisLife} ${LifeName}, ${ThisField} ${FieldName}, ${ThisDiff} ${DiffName}"
+    echo "Making files for ${ThisLife} ${LifeName}, ${ThisField} ${FieldName}, ${ThisDiff} ${DiffName}, ${ThisNoise} ${NoiseName}"
 
 ### Make the G4 file
 ### Note that this is using the counter muon GEANT4 fcl file!!!
-    cat <<EOF > ${MCTestFclDir}MCTest_g4_${LifeName}_${FieldName}_${DiffName}.fcl
+    cat <<EOF > ${MCTestFclDir}MCTest_g4_${LifeName}_${FieldName}_${DiffName}_${NoiseName}.fcl
 #include "standard_g4_dune35t_countermu.fcl"
 
 services.DetectorPropertiesService.Electronlifetime: ${ThisLife}
@@ -81,38 +101,39 @@ EOF
 
 ### Make the detsim file
 ### Note that this is using the millblock detsim module!!!
-    cat <<EOF > ${MCTestFclDir}MCTest_detsim_${LifeName}_${FieldName}_${DiffName}.fcl
+    cat <<EOF > ${MCTestFclDir}MCTest_detsim_${LifeName}_${FieldName}_${DiffName}_${NoiseName}.fcl
 #include "standard_detsim_dune35t_milliblock.fcl"
 
 services.DetectorPropertiesService.Electronlifetime: ${ThisLife}
 services.DetectorPropertiesService.Efield: ${ThisField}
 services.LArG4Parameters.LongitudinalDiffusion: ${ThisDiff}
 
+services.ChannelNoiseService: @local::chnoiseold
+
 EOF
-    if [ $1 -ne 4 ]; then
-	echo "services.ChannelNoiseService: @local::chnoiseold" >> ${MCTestFclDir}MCTest_detsim_${LifeName}_${FieldName}_${DiffName}.fcl
-    else
-	if [ $2 -eq 1 ]; then
-	    echo "" >> ${MCTestFclDir}MCTest_detsim_${LifeName}_${FieldName}_${DiffName}.fcl
-	elif [ $2 -eq 2 ]; then
-	    echo "" >> ${MCTestFclDir}MCTest_detsim_${LifeName}_${FieldName}_${DiffName}.fcl
-	elif [ $2 -eq 3 ]; then
-	    echo "" >> ${MCTestFclDir}MCTest_detsim_${LifeName}_${FieldName}_${DiffName}.fcl
-	elif [ $2 -eq 4 ]; then
-	    echo "" >> ${MCTestFclDir}MCTest_detsim_${LifeName}_${FieldName}_${DiffName}.fcl
-	fi
+    if [ $1 -eq 4 ]; then
+	cat<<EOF >> ${MCTestFclDir}MCTest_detsim_${LifeName}_${FieldName}_${DiffName}_${NoiseName}.fcl
+services.ChannelNoiseService.NoiseNormU: ${ThisNoise} # 3.16
+services.ChannelNoiseService.NoiseNormU: ${ThisNoise} # 3.16
+services.ChannelNoiseService.NoiseNormU: ${ThisNoise} # 3.16
+
+EOF
     fi
     
 ### Make the G4 file
 ### Note that this is using the counter muon GEANT4 fcl file!!!
-    cat <<EOF > ${MCTestFclDir}MCTest_reco_${LifeName}_${FieldName}_${DiffName}.fcl
+    cat <<EOF >> ${MCTestFclDir}MCTest_reco_${LifeName}_${FieldName}_${DiffName}_${NoiseName}.fcl
 #include "standard_reco_dune35tsim_milliblock.fcl"
 
 services.DetectorPropertiesService.Electronlifetime: ${ThisLife}
 services.DetectorPropertiesService.Efield: ${ThisField}
 
 EOF
-
+    if [ $1 -eq 4 ]; then
+	cat <<EOF > ${MCTestFclDir}MCTest_reco_${LifeName}_${FieldName}_${DiffName}_${NoiseName}.fcl
+physics.producers.gaushit.MinSig: ${ThisHitTh} # [ 5, 5, 10 ]
+EOF
+    fi
 }
 
 ### Make the fcl files for the different electron lifetimes.
@@ -131,4 +152,10 @@ done
 for (( diff=1; diff<${DiffLen}+1; diff++ ));
 do
     MakeTheFcls 3 ${diff}
+done
+
+### Now make the fcl files for the different noise levels.
+for (( noise=1; noise<${NoiseLen}+1; noise++ ));
+do
+    MakeTheFcls 4 ${noise}
 done
